a277141490d028fff782138f45c3869dfc05c0d6
Yuriy Kurbatov <kurbat1@gmail.com>
2021-12-09 16:54:16 +0400

    git-rebase 2
    Merge branch 'git-merge'

--- 02-git-03-branching/branching/rebase.sh
+++ 02-git-03-branching/branching/rebase.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # display command line options
-
-count=1
-for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
-
-echo "====="
+  
+  count=1
+  for param in "$@"; do
+      echo "Next parameter: $param"
+      count=$(( $count + 1 ))
+  done
+  
+  echo "====="
